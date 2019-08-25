2680471fa44bcbc366a8cff72c2117790176e0ae
Alex Gomes <alex.gomes.net3@gmail.com>
2019-08-25 17:46:15 -0300

    Final Commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Building clone interface Pipefy, using React js, library react-dnd, immer and React Context API. All credits video tutorial
+Youtube Channel RocketSeat, Teacher Diogo Fernandes.
+Link: https://youtu.be/awRtgpRsdTQ
+RocketSeat: https://rocketseat.com.br/ 
 
 ## Available Scripts
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,15 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from "react-dnd";
 
+import BoardContext from "../Board/context";
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef ] = useDrag({
-    item: { type: 'CARD', id: data.id },
+    item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -16,8 +18,35 @@ export default function Card({ data }) {
   const [, dropRef ] = useDrop({
     accept: 'CARD',
     hover(item, monitor){
-      console.log(item.id);
-      console.log(data.id);
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex; 
+
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+        return;
+      }
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+      
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+
+      move(draggedListIndex, draggedIndex, targetListIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
+
     }
   });
 
