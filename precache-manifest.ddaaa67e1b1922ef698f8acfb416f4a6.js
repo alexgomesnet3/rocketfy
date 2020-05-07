778b46a708f98d431eebf290f7a57f7ff8745c24
Alex Gomes <alex.gomes.net3@gmail.com>
2020-05-07 20:36:13 -0300

    Updates

---
+++ precache-manifest.ddaaa67e1b1922ef698f8acfb416f4a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2c401ad33cf141b3a4960d7f522899",
+    "url": "/rocketfy/index.html"
+  },
+  {
+    "revision": "6cbf0c87f01042a0b1ed",
+    "url": "/rocketfy/static/js/2.c75d8aa9.chunk.js"
+  },
+  {
+    "revision": "ad80e72edc94e7cbe8c5",
+    "url": "/rocketfy/static/js/main.5655cb0b.chunk.js"
+  },
+  {
+    "revision": "0fee6862b84f884a108b",
+    "url": "/rocketfy/static/js/runtime~main.395c8bfa.js"
+  }
+]);
